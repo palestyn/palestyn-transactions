22446ca693f3c4c3edbdfcc45fe04ce7e10ebb9f
Mehdi Raza <mehdi.reza@gmail.com>
2019-08-14 19:07:20 +0500

    doWithEntityManager with Function<EntityManager, R>

--- src/main/java/org/palestyn/tranasaction/impl/TransactionManagerImpl.java
+++ src/main/java/org/palestyn/tranasaction/impl/TransactionManagerImpl.java
@@ -3,6 +3,7 @@ package org.palestyn.tranasaction.impl;
 import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Instance;
@@ -15,6 +16,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceException;
 import javax.persistence.RollbackException;
 import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 import javax.transaction.TransactionalException;
 
 import org.palestyn.transaction.TransactionManager;
@@ -91,6 +93,35 @@ public final class TransactionManagerImpl implements TransactionManager {
 				throw (PersistenceException)e;
 			else throw new PersistenceException("Error while performing transaction, check the cause", e);
 		}
+	}
+	
+	@Override
+	public <R> R doWithEntityManager(TxType scope, Function<EntityManager, R> callback) {
+		
+		EntityManager _em = getScopedEntityManager(scope);
+
+		Either<Throwable, R> either = Try.ofSupplier(() -> {
+			return callback.apply(_em);
+		}).onFailure(PersistenceException.class, pe -> {
+			rollback.accept(_em, true);
+		}).andThenTry(() -> {
+			commit.accept(_em);
+		}).onFailure(RollbackException.class, rbe -> {
+			rollback.accept(_em, false);
+		}).andFinallyTry(() -> {
+			close.accept(_em);
+			em.destroy(_em);
+		}).toEither();
+		
+		Throwable e = either.swap().getOrNull();
+		
+		if(Objects.nonNull(e)) {
+			if(e instanceof PersistenceException)
+				throw (PersistenceException)e;
+			else throw new PersistenceException("Error while performing transaction, check the cause", e);
+		}
+		
+		return either.get();
 	}
 	
 	private EntityManager getScopedEntityManager(Transactional.TxType scope) {
